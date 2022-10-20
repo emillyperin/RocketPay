8c7d6d5440d757ce79aced64edb47e27f6094896
emillyperin <emilyperin5@gmail.com>
2022-10-20 20:30:06 -0300

    feat: add IMask

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import "./css/index.css"
+import IMask from 'imask'
+
+
+const bgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const bgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+    
+    
+function setCardType(type) {
+    const colors = {
+        visa: ["#436D99","#2D57F2"],
+        mastercard: ["#DF6F29","#C69347"],
+        default: ["black","gray"]
+    }
+
+    bgColor01.setAttribute("fill", colors[type][0])
+    bgColor02.setAttribute("fill", colors[type][1])
+    ccLogo.setAttribute("src", `cc-${type}.svg`)
+        
+}
+
+// Security code
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+    mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
