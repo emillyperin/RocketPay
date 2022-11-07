24c48eb07e8735e2f520fb57b6fb944c7118a7e1
emillyperin <emilyperin5@gmail.com>
2022-11-07 11:45:45 -0300

    feat: part1 modalSuccess

--- src/main.js
+++ src/main.js
@@ -92,7 +92,7 @@ const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
 
 const addButton = document.querySelector("#add-card")
 addButton.addEventListener("click", () => {
-  alert("Cartão adicionado!")
+  document.querySelector(".modal-form").classList.add("active")
 })
 
 document.querySelector("form").addEventListener("submit", (event) => {
